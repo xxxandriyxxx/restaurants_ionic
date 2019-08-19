6f0919f54d8c297e2dce08ad4a0411a958abcb02
Andriy <asoha474@gmail.com>
2019-08-20 01:05:58 +0300

    changed API URL

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -8,7 +8,7 @@ export class DataService {
 
     // URL of the API
     // public url = 'http://localhost:8080';
-    public url = 'http://ec2-52-15-125-215.us-east-2.compute.amazonaws.com:8080';
+    public url = 'http://ec2-18-217-248-193.us-east-2.compute.amazonaws.com:8080';
 
     public passLoginRegExp = new RegExp('^[a-zA-Z0-9]{3,20}$');
     public emailRegExp = new RegExp('^([a-z0-9_-]+\\.)*[a-z0-9_-]+@[a-z0-9_-]+(\\.[a-z0-9_-]+)*\\.[a-z]{2,6}$');
